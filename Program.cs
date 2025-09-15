e91a0d090e7bb66273aa051a01e0f0c6025fd2f1
beldonholt <beldon.holt@gmail.com>
2025-09-16 10:48:47 +1200

    added fun things function

--- Program.cs
+++ Program.cs
@@ -7,3 +7,13 @@ Console.WriteLine("Hello, World!");
 
 
 //------------------------------------------Celeste
+static void funThings()
+{
+    Console.WriteLine("hi I'm fun things");
+    Console.WriteLine("i can do many things, like counting to 100. See!");
+    for (int i = 0; i < 100; i++)
+    {
+        Console.WriteLine($"{i}");
+    }
+}
+funThings();
