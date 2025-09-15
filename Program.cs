66b103b90d1dae4c799dfb02a1582c6260c1248f
Himanshu Yadav (1000138607) <YADAH3@student.op.ac.nz>
2025-09-16 11:06:33 +1200

    Added a lab wor k

--- Program.cs
+++ Program.cs
@@ -7,6 +7,48 @@ Console.WriteLine("Hello to our github project");
 Console.WriteLine("In this we are three peoples working on same project");
 Console.WriteLine("We have created a new repo");
 
+namespace LAB6
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Enter Number1");
+            string input = Console.ReadLine();
+            int Number1 = int.Parse(input);
+
+            Console.WriteLine("Enter Number2");
+            string input2 = Console.ReadLine();
+            int Number2 = int.Parse(input2);
+
+            Console.WriteLine("Enter Number3");
+            string input3 = Console.ReadLine();
+            int Number3 = int.Parse(input3);
+
+            // check ascending order
+
+            if ((Number1 < Number2) && (Number2 < Number3))
+            {
+                Console.WriteLine($"Your numbers are in ascending order: {Number1} {Number2} {Number3}");
+            }
+            else
+            {
+                if ((Number1 > Number2) && (Number2 > Number3))
+                {
+                    Console.WriteLine("Numbers are entered in  descending order");
+                }
+                else
+                {
+                    Console.WriteLine("Numbers are not in order");
+                }
+            }
+
+
+            Console.ReadLine();
+        }
+    }
+}
+
 
 //------------------------------------------Rami
 RamiGreeting();
