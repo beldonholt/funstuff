5dbfab311fa1b5990837184ef162af33f238f51b
Rami Allo (1000056381) <ALLORM1@student.op.ac.nz>
2025-09-16 10:51:09 +1200

    add ramigreeting

--- Program.cs
+++ Program.cs
@@ -1,9 +1,16 @@
 ﻿// See https://aka.ms/new-console-template for more information
+using System;
+
 Console.WriteLine("Hello, World!");
 //------------------------------------------Himanshu
 
 
 //------------------------------------------Rami
+RamiGreeting();
 
+// Method defined for Rami
+void RamiGreeting()
+{
+    Console.WriteLine("Hello from Rami!");
 
 //------------------------------------------Celeste
